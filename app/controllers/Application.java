091bf8727e323f2b75ed4423d1c300a6cb49ff40
faulomi <jerome.meuret@gmail.com>
2013-05-23 23:00:36 +0200

    Deezer to spotify is working.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,16 @@
 package controllers;
 
 
-import play.*;
-
+import org.codehaus.jackson.JsonNode;
 import play.libs.WS;
 import play.libs.F;
-import play.libs.WS;
 import play.mvc.*;
 
 
 import views.html.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static play.libs.WS.*;
 
@@ -26,56 +27,122 @@ public class Application extends Controller {
         }
     }
 
+    public static class SearchParameters {
+
+        public String url;
+        public final Map<String, String> queryParameters = new HashMap<String, String>();
+    }
+
+
+    public enum Provider {
+        DEEZER("https://api.deezer.com/2.0/", "https://api.deezer.com/2.0/search") {
+            @Override
+            public SearchParameters buildSearchParameters(Track track) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public String buildLookupUrl(String type, String id) {
+                return lookupBaseUrl + type +"/" +id;
+            }
+
+            @Override
+            public Track getTrack(JsonNode root) {
+                String title = root.findPath("title").asText();
+                String artist = root.findPath("artist").findPath("name").asText();
+                return new Track(title, artist);
+            }
+
+            @Override
+            public String getTrackUrl(JsonNode root) {
+
+                return null;
+            }
+        },
+        SPOTIFY("http://ws.spotify.com/lookup/1/", "http://ws.spotify.com/search/1/") {
+            @Override
+            public Track getTrack(JsonNode root) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public String buildLookupUrl(String type, String id) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public SearchParameters buildSearchParameters(Track track) {
+                SearchParameters searchParameters = new SearchParameters();
+                searchParameters.url = searchBaseUrl + "track.json";
+                searchParameters.queryParameters.put("q", track.artist + " " + track.title);
+
+                return searchParameters;
+            }
+
+            @Override
+            public String getTrackUrl(JsonNode root) {
+
+                return root.findPath("tracks").get(0).findValuesAsText("href").get(1);
+            }
+        };
+
+
+        private Provider(String lookupBaseUrl, String searchBaseUrl) {
+
+            this.searchBaseUrl = searchBaseUrl;
+            this.lookupBaseUrl = lookupBaseUrl;
+        }
+
+
+
+        public abstract Track getTrack(JsonNode root);
+        public abstract String buildLookupUrl(String type, String id);
+        public abstract SearchParameters buildSearchParameters(Track track);
+        public abstract String getTrackUrl(JsonNode root);
+        protected String lookupBaseUrl;
+
+        protected String searchBaseUrl;
+    }
+
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+
+
     }
 
 
     public static Result share(String source, final String type, String id, String destination) {
 
+        final Provider sourceProvider = Provider.valueOf(source);
+        final Provider destinationProvider = Provider.valueOf(destination);
 
+        return (async(url(sourceProvider.buildLookupUrl(type,id)).get().flatMap(new F.Function<WS.Response, F.Promise<Result>>() {
+            public F.Promise<Result> apply(WS.Response response) throws Throwable {
 
-            return (async(url("https://api.deezer.com/2.0/" + type + "/" + id).get().flatMap(new F.Function<WS.Response, F.Promise<Result>>() {
-                public F.Promise<Result> apply(WS.Response response) throws Throwable {
+                final Track track = sourceProvider.getTrack(response.asJson());
 
-                    String title = response.asJson().findPath("title").asText();
-                    String artist = response.asJson().findPath("artist").findPath("name").asText();
+                final SearchParameters searchParameters = destinationProvider.buildSearchParameters(track);
+                WSRequestHolder request =  url(searchParameters.url);
+                for(Map.Entry<String, String> parameter : searchParameters.queryParameters.entrySet()){
 
-                    return url("http://ws.spotify.com/search/1/" + type + ".json").setQueryParameter("q", artist + " " + title).get().map(new F.Function<Response, Result>() {
-                        @Override
-                        public Result apply(Response response) throws Throwable {
-
-
-                            return ok(response.asJson().findPath("tracks").get(0));
-                        }
-                    });
+                        request.setQueryParameter(parameter.getKey(), parameter.getValue());
                 }
-            })));
+                return request.get().map(new F.Function<Response, Result>() {
+                    @Override
+                    public Result apply(Response response) throws Throwable {
 
 
-        }
-
-
-
-
-
-
-
-    public static Result searchOnSpotify(final String track) {
-
-
-        final F.Promise<Result> resultPromise = url("https://api.deezer.com/2.0/track/" + track).get().map(new F.Function<Response, Result>() {
-            @Override
-            public Result apply(Response response) throws Throwable {
-                return ok(response.asJson());
+                        return redirect(destinationProvider.getTrackUrl(response.asJson()));
+                    }
+                });
             }
-        });
-
-        return async(resultPromise);
+        })));
 
 
     }
+
+
 
 
 }
